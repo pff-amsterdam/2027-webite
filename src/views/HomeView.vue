f334e39f29a56753def409710dd0459ff8cc1804
Meggie Veggie <megs.kamp@icloud.com>
2026-07-30 13:12:37 +0200

    update image

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -25,13 +25,6 @@ const submissionLinks = [
 <template>
   <main class="hero" :style="{ backgroundImage: `url(${heroBackground})` }">
     <div class="hero-overlay">
-      <h1 class="hero-title">
-        Porn<br />
-        Film<br />
-        Festival<br />
-        Amsterdam
-      </h1>
-
       <div class="hero-buttons">
         <a
           v-for="link in submissionLinks"
@@ -69,7 +62,7 @@ const submissionLinks = [
   display: flex;
   flex-wrap: wrap;
   align-items: center;
-  justify-content: space-between;
+  justify-content: flex-end;
   gap: 4rem;
 }
 
